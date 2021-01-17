c5094fe235afb41ff2e02fbd4083f5db578af412
anling <a1265478@gmail.com>
2021-01-17 18:48:27 +0800

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Menu from './Menu/Menu'
 import MenuDirector from './Redirect/MenuDirector'
 import './const'
 import OrderDetails from './Orders/OrderDetails';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div>
         <Route exact path="/" component={Menu} />
         <Route path="/menu" component={Menu} />
         <Route path="/orders" component={Orders} />
         <Route path="/details" component={OrderDetails} />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
